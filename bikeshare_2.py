3bda9328d3d9ec6acd038c9ba13c1ab60c10aa95
Isrrael Mendoza <isrraelmendoza92@gmail.com>
2022-10-02 13:51:13 -0700

    if loop to ensure that user is prompted for a restart

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -130,6 +130,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+#if loop to ensure that user is prompted for a restart
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
